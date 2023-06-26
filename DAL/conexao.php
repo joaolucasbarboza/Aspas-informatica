e1eb7d2967b1047a67e1d767a12eeaba53cf4186
João L. Barboza <105928845+valladev@users.noreply.github.com>
2023-06-26 09:36:12 -0300

    dal conexao

---
+++ DAL/conexao.php
@@ -0,0 +1,32 @@
+<?php 
+namespace DAL;
+
+class Conexao {
+    private static $dbName = 'aspas_informatica';
+    private static $dbHost = 'localhost';
+    private static $dbUser = 'root';
+    private static $dbPassword = '';
+    private static $cont = null;
+
+    public function __construct() {
+        die('A função init n˜o é permitida');
+    }
+
+    public static function conectar() {
+        if (self::$cont == null) {
+            try {
+                self::$cont = new \PDO("mysql:host".self::$dbHost.";dbName=".self::$dbName, self::$dbUser, self::$dbPassword);
+            }
+            catch (\PDOException $exeption) {
+                die($exeption->getMessage());
+            }
+        }
+        return self::$cont;
+    }
+
+    public static function desconectar() {
+        self::$cont = null;
+    }
+}
+
+?>
