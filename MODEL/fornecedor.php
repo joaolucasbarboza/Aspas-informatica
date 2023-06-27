38b76417fa735dd1a8f0e2c7d0683b7d9746ce59
João L. Barboza <105928845+valladev@users.noreply.github.com>
2023-06-27 12:52:18 -0300

    classe fornecedor

---
+++ MODEL/fornecedor.php
@@ -0,0 +1,45 @@
+<?php 
+
+namespace MODEL;
+
+class Fornecedor {
+    private ?int $id;
+    private ?string $razaoSocial;
+    private ?string $cnpj;
+    private ?string $email;
+
+    public function __construct() {
+
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getRazaoSocial() {
+        return $this->razaoSocial;
+    }
+
+    public function getCnpj() {
+        return $this->cnpj;
+    }
+
+    public function getEmail() {
+        return $this->email;
+    }
+
+    public function setId(int $id) {
+        return $this->id = $id;
+    }
+
+    public function setRazaoSocial(string $razaoSocial) {
+     return $this->razaoSocial = $razaoSocial;   
+    }
+
+    public function setEmail(string $email) {
+        return $this->email = $email;
+    }
+
+}
+
+?>
